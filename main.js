36d232149ab494a3176768c6fe35ec4795f935a2
12101111 <w12101111@gmail.com>
2017-01-24 22:16:47 +0800

    add style

--- main.js
+++ main.js
@@ -2,40 +2,41 @@ var canvas = document.getElementById('main_canvas'),
     context = canvas.getContext('2d');
 var colony = {
     map: [], //x,y,size,camp,type,population
+    color: ["DimGray", "Orchid", "SpringGreen", "OrangeRed", "DodgerBlue", "Black"],
     init: function () {
-        context.canvas.width = 800;
-        context.canvas.height = 600;
+        context.canvas.width = window.innerWidth-60;
+        context.canvas.height = window.innerHeight-80;
+        context.lineWidth = 2;
         context.font = '30pt Arial';
         context.fillStyle = 'cornflowerblue';
         context.strokeStyle = 'black';
         context.fillText("testing!", 50, 50);
+        window.addEventListener("resize", function () {
+            context.canvas.width = window.innerWidth-60;
+            context.canvas.height = window.innerHeight-90;
+            colony.updateMap();
+        })
         this.loadMap();
     },
     updateMap: function () {
         var zoom_x = context.canvas.width / 80;
         var zoom_y = context.canvas.height / 60;
-        context.clearRect(0,0,canvas.width,canvas.height);
+        context.clearRect(0, 0, canvas.width, canvas.height);
         this.map.forEach(
-            function(star) {
+            function (star) {
                 var x = zoom_x * star[0],
                     y = zoom_y * star[1],
                     r = 5 * star[2],
-                    camp = star[3],
-                    type = star[4];
+                    populationStr = (star[5]).toString(),
+                    textWidth = context.measureText(populationStr).width; //some bug in first run
+                //type = star[4]; //only one type now
                 context.beginPath();
-                context.arc(x, y, r + 10, 0, 2 * Math.PI, true);
+                context.arc(x, y, r + 10, 0, 2 * Math.PI, true); //only when type==1
                 context.stroke();
-                switch (camp) {
-                    case 1:
-                        context.fillStyle = "green";
-                        break;
-                    case 2:
-                        context.fillStyle = "red";
-                        break;
-                }
+                context.fillStyle = colony.color[star[3]];
                 context.fill();
                 context.font = '10pt Arial';
-                context.fillText((star[5]).toString(), x + r - 5, y + r + 25);
+                context.fillText(populationStr, x - textWidth / 2, y + r + 25);
             }
         );
     },
@@ -50,15 +51,14 @@ var colony = {
             }
             $("#startBtn").after(text);
             $("button.choose_map").click(function () {
-                map_n=parseInt($(this).attr("id").substring(3));
-                colony.map = data.maps[map_n-1];
-                colony.map.forEach(function(star){
-                    if(star[3]!=0&&star[5]==0)
-                    {
-                        star[5]=star[2]*10;
+                map_n = parseInt($(this).attr("id").substring(3));
+                colony.map = data.maps[map_n - 1];
+                colony.map.forEach(function (star) {
+                    if (star[3] != 0 && star[5] == 0) {
+                        star[5] = star[2] * 10;
                     }
                 });
-                colony.updateMap();//Should change
+                colony.updateMap(); //Should change
                 $("button.choose_map").hide();
                 $("#choose_tip").hide();
             });
