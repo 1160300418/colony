2c21a2bb7a3fa010eb0f5ad6019df78d6a35460c
12101111 <w12101111@gmail.com>
2017-02-03 22:25:54 +0800

    add background support fix a bug

--- main.js
+++ main.js
@@ -1,34 +1,41 @@
 var canvas = document.getElementById('main_canvas'),
-    context = canvas.getContext('2d');
+    context = canvas.getContext('2d'),
+    background = new Image();
+background.src = 'background.jpg';
+background.onload = function (e) {
+    context.drawImage(background, 0, 0);
+};
 var colony = {
     map: [], //x,y,size,camp,type,population
     color: ["DimGray", "Orchid", "SpringGreen", "OrangeRed", "DodgerBlue", "Black"],
     init: function () {
-        context.canvas.width = window.innerWidth-60;
-        context.canvas.height = window.innerHeight-80;
+        context.canvas.width = window.innerWidth - 60;
+        context.canvas.height = window.innerHeight - 80;
+        if (background.complete)
+            context.drawImage(background, 0, 0, context.canvas.width, context.canvas.height);
         context.lineWidth = 2;
         context.font = '30pt Arial';
         context.fillStyle = 'cornflowerblue';
         context.strokeStyle = 'black';
-        context.fillText("testing!", 50, 50);
-        window.addEventListener("resize", function () {
-            context.canvas.width = window.innerWidth-60;
-            context.canvas.height = window.innerHeight-90;
-            colony.updateMap();
-        })
+        context.fillText("alpha version!", 50, 50);
         this.loadMap();
     },
     updateMap: function () {
+        context.canvas.width = window.innerWidth - 60;
+        context.canvas.height = window.innerHeight - 80;
         var zoom_x = context.canvas.width / 80;
         var zoom_y = context.canvas.height / 60;
         context.clearRect(0, 0, canvas.width, canvas.height);
+        if (background.complete)
+            context.drawImage(background, 0, 0, context.canvas.width, context.canvas.height);
         this.map.forEach(
             function (star) {
                 var x = zoom_x * star[0],
                     y = zoom_y * star[1],
                     r = 5 * star[2],
-                    populationStr = (star[5]).toString(),
-                    textWidth = context.measureText(populationStr).width; //some bug in first run
+                    populationStr = (star[5]).toString();
+                //console.log(populationStr);
+                var textWidth = context.measureText(populationStr).width; //some bug in first run
                 //type = star[4]; //only one type now
                 context.beginPath();
                 context.arc(x, y, r + 10, 0, 2 * Math.PI, true); //only when type==1
@@ -41,27 +48,35 @@ var colony = {
         );
     },
     loadMap: function () {
-        $.getJSON("map.json", function (data) {
-            $("#startBtn").hide();
-            var len = data.maps.length;
-            var text = "<h3 id=\"choose_tip\">Choose a map.</h3><p>";
-            var map_n = 0;
-            for (var i = 1; i <= len; i++) {
-                text += "<button type=\"button\" class=\"choose_map\" id=\"map" + i + "\">" + i + "</button>"
-            }
-            $("#startBtn").after(text);
-            $("button.choose_map").click(function () {
-                map_n = parseInt($(this).attr("id").substring(3));
-                colony.map = data.maps[map_n - 1];
-                colony.map.forEach(function (star) {
-                    if (star[3] != 0 && star[5] == 0) {
-                        star[5] = star[2] * 10;
-                    }
+        try {
+            $.getJSON("map.json", function (data) {
+                $("#startBtn").hide();
+                var len = data.maps.length;
+                var text = "<h3 id=\"choose_tip\">Choose a map.</h3><p>";
+                var map_n = 0;
+                for (var i = 1; i <= len; i++) {
+                    text += "<button type=\"button\" class=\"choose_map\" id=\"map" + i + "\">" + i + "</button>"
+                }
+                $("#startBtn").after(text);
+                $("button.choose_map").click(function () {
+                    map_n = parseInt($(this).attr("id").substring(3));
+                    colony.map = data.maps[map_n - 1];
+                    colony.map.forEach(function (star) {
+                        if (star[3] != 0 && star[5] == 0) {
+                            star[5] = star[2] * 10;
+                        }
+                    });
+                    colony.updateMap(); //Should change
+                    $("button.choose_map").hide();
+                    $("#choose_tip").hide();
                 });
-                colony.updateMap(); //Should change
-                $("button.choose_map").hide();
-                $("#choose_tip").hide();
+                window.addEventListener("resize", function () {
+                    colony.updateMap();
+                })
             });
-        });
+        } catch (e) {
+            console.error("Can\'t load map.json!");
+        }
     },
 }
+//window.onload = colony.init();
