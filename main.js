3ef4e84696c403a99df1210b716c5a1b70c1abf1
12101111 <w12101111@gmail.com>
2017-02-11 10:13:56 +0800

    some changes on shipMove();

--- main.js
+++ main.js
@@ -35,16 +35,13 @@ var colony = {
                 console.error("Can\'t load map.json!");
             }
         },
-        shipMove: function (i) {
-            var from = colony.lastSelect,
-                to = i;
-            colony.lastSelect = undefined;
+        shipMove: function (from,to,authority) {
             if (from == to) return;
-            if (!colony.map[from][5][colony.camp]) return;
+            if (!colony.map[from][5][authority]) return;
             //animation callback
-            if (!colony.map[to][5][colony.camp]) colony.map[to][5][colony.camp] = 0;
-            colony.map[to][5][colony.camp] += colony.map[from][5][colony.camp] * colony.shipRatio;
-            colony.map[from][5][colony.camp] *= (1 - colony.shipRatio);
+            if (!colony.map[to][5][authority]) colony.map[to][5][authority] = 0;
+            colony.map[to][5][authority] += colony.map[from][5][authority] * colony.shipRatio;
+            colony.map[from][5][authority] *= (1 - colony.shipRatio);
         },
         combat: function () {
 
@@ -61,6 +58,8 @@ var colony = {
             colonyUI.drawBackground();
             context.font = '30pt Arial';
             context.fillStyle = 'cornflowerblue';
+            context.textAlign = "center";
+            context.textBaseline = "middle";
             colony.loadMap();
         },
         chooseMapUI: function (data, dataInit) {
@@ -106,6 +105,7 @@ var colony = {
                     zoom_y = context.canvas.height / ZOOM_ON_Y;
                 var x = zoom_x * colony.map[colony.lastSelect][0],
                     y = zoom_y * colony.map[colony.lastSelect][1];
+                context.strokeStyle = "black";
                 context.lineWidth = 2;
                 context.beginPath();
                 context.arc(x, y, 10 * colony.map[colony.lastSelect][2] + 50, 0, 2 * Math.PI, true);
@@ -120,36 +120,39 @@ var colony = {
                 y = zoom_y * star[1];
             //type = star[4]; //only one type now
             context.lineWidth = 2;
+            context.fillStyle = colonyUI.color[star[3]];
+            context.strokeStyle = "black";
             context.beginPath();
             context.arc(x, y, 10 * star[2] + 10, 0, 2 * Math.PI, true); //only when type==1//magic number
             context.stroke();
-            context.fillStyle = colonyUI.color[star[3]];
             context.fill();
             context.font = '10pt Arial';
             context.fillStyle = "black";
-            context.fillText("index:" + index, x - 20, y + 5); //test only
-            var totalCampNum = 0,
-                campCount = 0,
-                totalPopulation = 0,
-                populationCount;
+            context.textAlign = "center";
+            context.textBaseline = "middle";
+            context.fillText("index:" + index, x , y); //test only
+            var totalPopulation = 0;
+                
             for (var i = 0, len = star[5].length; i < len; i++) {
                 if (!star[5][i]) continue;
-                totalCampNum++;
                 totalPopulation += star[5][i];
             }
-            star[5].forEach(function (text, i) { //many bugs
-                if (!text) return;
+            for(var i=0,len=star[5].length,populationCount = 0;i<len;i++)
+            {
+                text=star[5][i];
+                if (!text) continue;
                 var populationStr = text.toString(),
-                    textWidth = context.measureText(populationStr).width;
+                     textWidth = context.measureText(populationStr).width;
                 context.fillStyle = colonyUI.color[i];
-                context.fillText(populationStr, x + (10 * star[2] + 10) * Math.sin(campCount / totalCampNum * Math.PI * 2) - textWidth / 2, y + (10 * star[2] + 10) * Math.cos(campCount / totalCampNum * Math.PI * 2) + 15); //magic number
-                context.lineWidth = 4;
-                context.beginPath();
-                context.arc(x, y, 10 * star[2] + 15, (populationCount / totalPopulation * 2 - 0.5) * Math.PI, ((populationCount + star[5][i]) / totalPopulation * 2 - 0.5) * Math.PI, true);
+                context.strokeStyle = colonyUI.color[i];
+                context.lineWidth = 10;
+                context.beginPath();//bug here
+                context.arc(x, y, 10 * star[2] + 35, populationCount / totalPopulation * 2  * Math.PI, (populationCount + text) / totalPopulation * 2  * Math.PI, true);
                 context.stroke();
-                campCount++;
-                populationCount += star[5][i];
-            });
+                context.lineWidth = 2;
+                context.fillText(populationStr, x + (10 * star[2] + 20) * Math.cos((populationCount + text/2) / totalPopulation * Math.PI * 2) , y - (10 * star[2] + 20) * Math.sin((populationCount + star[5][i]/2) / totalPopulation * Math.PI * 2));
+                populationCount += text;
+            }
         },
         drawShip: function (ship) {},
         select: function (e) {
@@ -168,6 +171,7 @@ var colony = {
             var y = zy / zoom_y; //test only
             document.getElementById("input_canvas").value = zx + "," + zy;
             document.getElementById("input_map").value = parseInt(x) + "," + parseInt(y); //test only
+            if (pause) return;
             var match = false;
             for (var i = 0, len = colony.map.length; i < len; i++) {
                 star = colony.map[i];
@@ -177,7 +181,8 @@ var colony = {
                     if (typeof (colony.lastSelect) == 'undefined') {
                         colony.lastSelect = i;
                     } else {
-                        colony.shipMove(i);
+                        colony.shipMove(colony.lastSelect,i,colony.camp);
+                        colony.lastSelect = undefined;
                     }
                     match = true;
                     break;
@@ -187,7 +192,7 @@ var colony = {
                 document.getElementById("input_select").value = "";
                 colony.lastSelect = undefined;
             }
-            document.getElementById("input_select").value += colony.lastSelect ? colony.lastSelect : "none";
+            document.getElementById("input_select").value += typeof (colony.lastSelect) == 'undefined' ? colony.lastSelect : "none";
         },
         distance: function (x1, y1, x2, y2) {
             return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
